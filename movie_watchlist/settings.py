bed78f77b4597c13c7bc67c905999819f6147452
DARSH MISHRA <darshmishra@DARSHs-MacBook-Air.local>
2025-06-17 21:48:51 -0400

    Troubleshooting Render deployment

--- movie_watchlist/settings.py
+++ movie_watchlist/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'movie_watchlist.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -139,4 +139,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
